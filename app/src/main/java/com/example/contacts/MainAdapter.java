19b432596de90d61cf5ef64dd3b39bc4c2da710b
Soujanya <sujusoujanya23@gmail.com>
2022-07-08 13:42:27 +0530

    splash page added

---
+++ app/src/main/java/com/example/contacts/MainAdapter.java
@@ -0,0 +1,63 @@
+package com.example.contacts;
+
+import android.app.Activity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+
+public class MainAdapter extends RecyclerView.Adapter<MainAdapter.ViewHolder> {
+    Activity activity;
+    ArrayList<ContactModel> arrayList;
+
+    public MainAdapter(Activity activity, ArrayList<ContactModel> arrayList) {
+        this.activity = activity;
+        this.arrayList = arrayList;
+        notifyDataSetChanged();
+    }
+
+    @NonNull
+    @Override
+
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_contact,parent,false);
+
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull MainAdapter.ViewHolder holder, int position) {
+ContactModel model = arrayList.get(position);
+holder.tvName.setText(model.getName());
+holder.tvPhone.setText(model.getPhone());
+    }
+
+
+    @Override
+    public int getItemCount() {
+        return arrayList.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+        TextView tvName,tvPhone;
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+            tvName = itemView.findViewById(R.id.tv_name);
+            tvPhone = itemView.findViewById(R.id.tv_phone);
+        }
+    }
+}
+
+
+
+
+
+
+
+
